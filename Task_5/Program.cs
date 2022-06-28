eb1cc62e1208c2129932570d8c5c386da643e647
OlgaS <roga&kopita@mail.ru>
2022-06-28 17:09:19 +0500

    add task_5/7

---
+++ Task_5/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая на вход принимает
+//одно число (N), а на выходе показывает все целые
+//числа в промежутке от -N до N.
+Console.Write("Введите положительное число");
+double a = Convert.ToInt32(Console.ReadLine());
+double n = -a;
+int c = (int)n;
+if (a>0)
+{
+    while (c <= a)
+    {
+       Console.Write(" " + c);
+      c += 1;
+    }
+}
+else Console.Write("Error ");
