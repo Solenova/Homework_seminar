2a2ef287ce23b3e9f36965b58e9be2c2aeffae63
OlgaS <roga&kopita@mail.ru>
2022-06-29 21:45:36 +0500

    add Task_12

---
+++ Task_12/Program.cs
@@ -0,0 +1,5 @@
+﻿int rndNum1 = new Random().Next(10, 100);
+int rndNum2 = new Random().Next(1, 10);
+int ost1 = rndNum1 % rndNum2;
+if (ost1 == 0) Console.WriteLine($"{rndNum1} кратно {rndNum2} ");
+else Console.WriteLine($"{rndNum1} не кратно {rndNum2}, остаток {ost1} ");
