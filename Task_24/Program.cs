c79bb1b60c80df429ee3ff1f0d83b8963405cc6f
OlgaS <roga&kopita@mail.ru>
2022-07-06 23:27:09 +0500

    сумму чисел от 1 до А

---
+++ Task_24/Program.cs
@@ -0,0 +1,22 @@
+﻿//напишите программу, ктороая принимает на вход число А и выдает сумму числе от 1 до А...
+Console.WriteLine("Введите число:");
+int num = int.Parse(Console.ReadLine());
+int sum = 0;
+// for (int i = 1; i <= num; i++)
+// {
+//     sum += i;
+// }
+// Console.WriteLine($"Сумма чисел от 1 до {num}={sum}");
+
+// Решение с помощью метода:
+int Sum(int number)
+{
+    int sum = 0;
+    for (int i = 1; i <= number; i++)
+    {
+        sum += i;
+    }
+return sum;
+}
+int result=Sum(num);
+Console.WriteLine($"Сумма чисел от 1 до {num}={result}");
