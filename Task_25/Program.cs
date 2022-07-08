bac58f228c037f3c41750a22611ac2d86fe709d2
OlgaS <roga&kopita@mail.ru>
2022-07-08 21:09:24 +0500

    натуральная степень числа

---
+++ Task_25/Program.cs
@@ -0,0 +1,35 @@
+﻿// Напишите цикл, который принимает на вход два числа (АиВ) 
+//и возводит число А в натуральную степень В.
+Console.Write("Введите число A: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите натуральное число B: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+void DegreeNum(int num, int degree)
+{
+    int step = 1;
+    int i = 0;
+    if (degree > 0)
+    {
+        while (i < degree)
+        {
+            try
+            {
+                checked
+                {
+                    step = step * num;
+                    i++;
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Переполнение типа!");
+                break;
+            }
+        }
+        Console.WriteLine($"Число  {num} в степени {degree}= {step}");
+
+    }
+    else Console.Write("Стпень числа должна быть натуральным числом");
+}
+DegreeNum(a, b);
