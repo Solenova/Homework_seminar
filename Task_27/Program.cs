0d47c45b05855b349b1aad514690ae7f0ff65b4c
OlgaS <roga&kopita@mail.ru>
2022-07-08 22:26:16 +0500

    cумма цифр в числе

---
+++ Task_27/Program.cs
@@ -0,0 +1,19 @@
+﻿//  Напишите программу, которая принимает на вход число 
+// и выдаёт сумму цифр в числе.
+Console.Write("Введите число A: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+int SumNam (int nam)
+{
+    nam=Math.Abs(nam);
+    int sum=0;
+    while (nam!=0)
+    {
+        int namost=nam%10;
+        nam=nam/10;
+        sum+=namost;
+    }
+    return sum;
+}
+int s=SumNam(a);
+Console.Write($"Сумма цифр числа {a} равна {s}");
