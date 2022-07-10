0094679bf1b89ff4bd0c4bfd2ef4839bc8a2a09f
OlgaS <roga&kopita@mail.ru>
2022-07-10 20:38:39 +0500

    количество четных элементов одномерного массива

---
+++ Task_34/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+Console.Clear();
+int[] CreatArrayRndInt(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i == 0) Console.Write("[");
+        if (i < arr.Length - 1) Console.Write(arr[i] + ",");
+        else Console.Write(arr[i] + "]");
+    }
+}
+
+int QuantityEvenArr(int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] % 2 == 0) count += 1;
+    }
+    return (count);
+}
+
+Console.Write("Введите количество элементов массива ");
+int length=Convert.ToInt32(Console.ReadLine());
+
+int[] array=CreatArrayRndInt(length,100,1000);
+PrintArray(array);
+int result=QuantityEvenArr(array);
+Console.WriteLine($"\n Количество четных элементов равно {result}");
