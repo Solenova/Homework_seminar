8274e4cbde421185ca7f88167c9fa6bdc3b15fc7
OlgaS <roga&kopita@mail.ru>
2022-07-12 23:07:47 +0500

    количество положительных чисел из введенных пользователем

---
+++ Task_41/Program.cs
@@ -0,0 +1,21 @@
+﻿// Пользователь вводит с клавиатуры M чисел.
+//  Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// -1, -7, 567, 89, 223-> 3
+Console.Clear();
+Console.WriteLine("Укажите сколько чисел Вы будете вводить ");
+int quantity = int.Parse(Console.ReadLine());
+
+void QuantityPositiv (int nam)
+{
+    int count=0;
+    for (int i=0; i<nam; i++)
+    {
+        Console.Write($"{i+1} число:");
+        int temp=int.Parse(Console.ReadLine());
+        //Console.WriteLine();
+        if (temp>0) count+=1;
+    }
+    Console.WriteLine($"Введено {count} положительных чисел ");
+}
+QuantityPositiv(quantity);
