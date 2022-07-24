98c53db4cef4472e3610e6424b0ce571eb1b6bcc
OlgaS <roga&kopita@mail.ru>
2022-07-24 15:10:44 +0500

    Сумма натуральных чисел от M до N. Рекурсия.

---
+++ Task_66/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 66: Задайте значения М и N. Напишите программу, которая найдет 
+//сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.Clear();
+Console.Write("Задайте начальное число ");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.Write("Задайте конечное число ");
+int numEnd = Convert.ToInt32(Console.ReadLine());
+
+int summ = SummNumber(num, numEnd);
+Console.WriteLine($"Сумма чисел от {num} до {numEnd} равна {summ}");
+
+int SummNumber(int n, int m)
+{
+    if (n < m) return SummNumber(n + 1, m) + n;
+    else if (n > m) return SummNumber(n - 1, m) + n;
+    return n;
+}
